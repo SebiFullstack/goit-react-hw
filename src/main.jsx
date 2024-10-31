55da69eb68ecce3631f8f641c5ca32ae4fe13fbc
Sebastian4chik <sebi.ashtefcules@gmail.com>
2024-10-31 20:20:12 +0000

    add files

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import "the-new-css-reset/css/reset.css";
+import "modern-normalize";
+import App from './components/App'
 import './index.css'
-import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
